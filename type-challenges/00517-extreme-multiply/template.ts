ef16e73fcf35a4d2d2916a673393a84d4e4f0d93
陈志豪 <chenzhihao@myzaker.com>
2022-07-20 09:41:47 +0800

    乘法

--- type-challenges/00517-extreme-multiply/template.ts
+++ type-challenges/00517-extreme-multiply/template.ts
@@ -1,8 +1,10 @@
-type Multiply<A extends string | number | bigint, B extends string | number | bigint> = ArrMultiply<Split<`${A}`, ''>, Split<`${B}`, ''>>
+type Multiply<A extends string | number | bigint, B extends string | number | bigint> = isZero<A, B> extends true ? '0' : ArrMultiply<Split<`${A}`, ''>, Split<`${B}`, ''>>
+
+type isZero<A extends string | number | bigint, B extends string | number | bigint> = `${A}` extends '0' ? true : `${B}` extends '0' ? true : false
 
 type ArrMultiply<A extends string[], B extends string[], fill extends string = '', R extends string = '', AL extends string = GetLast<A, ''>> = AL extends ''
   ? R
-  : ArrMultiply<Pop<A>, B, `0${fill}`, `${Sum<OneMultiplyMore<AL, B>, R>}${fill}`>
+  : ArrMultiply<Pop<A>, B, `0${fill}`, Sum<`${OneMultiplyMore<AL, B>}${fill}`, R>>
 
 type OneMultiplyMore<
   O extends string = '1',
@@ -10,7 +12,7 @@ type OneMultiplyMore<
   fill extends string = '',
   R extends string = '',
   S extends unknown[] = SingleMultiply<CountToT<O>, CountToT<GetLast<M, '1'>>>
-> = M extends [] ? R : OneMultiplyMore<O, Pop<M>, `0${fill}`, `${Sum<S['length'], R>}${fill}`>
+> = M extends [] ? R : OneMultiplyMore<O, Pop<M>, `0${fill}`, Sum<`${S['length']}${fill}`, R>>
 
 type SingleMultiply<A extends unknown[] = [], N extends unknown[] = [], Count extends 1[] = [], R extends unknown[] = []> = Count['length'] extends N['length']
   ? R
@@ -21,4 +23,4 @@ type testOneMultiplyMore = OneMultiplyMore<'9', ['2', '0', '0']>
 type testMultiply = Multiply<0, 16> // 00 ??
 type testMultiply2 = Multiply<259, 9125385> // 2363474715 = 188931028401420127800000000
 
-type testArrMultiply = ArrMultiply<['3', '1', '0'], ['1', '2', '1', '4']>
+type testArrMultiply = ArrMultiply<['3', '0', '0'], ['1', '2', '1', '4']>
